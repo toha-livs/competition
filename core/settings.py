9420ada6a381a8f1c153a64fd21cbfd2326a6660
anton <aghica@bk.ru>
2020-12-16 22:10:13 +0200

    add static

--- core/settings.py
+++ core/settings.py
@@ -124,8 +124,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 try:
     from core.local_settings import *
